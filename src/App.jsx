c913eac0f77612e79edce213d2dcc43abab21072
Piyush Agarwal <piyushagarwalvo@gmail.com>
2024-07-04 12:15:23 +0530

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>React 19 Tutorials</h1>
-      <p>Check the branched in this repo for lesson wise code</p>
+      <p>Check the branches in this repo for lesson wise code</p>
     </div>
   );
 }
